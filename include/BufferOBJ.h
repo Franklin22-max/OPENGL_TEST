36411cf3d580bd6d3610540b31aa812f9846af30
Franklin Ajaegbu <ajaegbufrank22@gmail.com>
2023-01-30 05:52:07 +0100

    little clean up

--- include/BufferOBJ.h
+++ include/BufferOBJ.h
@@ -130,7 +130,13 @@ public:
 // // texture binding for shador
 //glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
 
+/*
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
+*/
 
 class FrameBuffer
 {
@@ -198,7 +204,7 @@ class UniformBuffer
 	GLuint bindPoint;
 	std::string name;
 public:
-	UniformBuffer(GLuint bindPoint , std::string name, GLsizei size = 150)
+	UniformBuffer(GLuint bindPoint , std::string name, GLsizei size)
 		:bindPoint(bindPoint), name(name)
 	{
 		glGenBuffers(1, &ubo);
